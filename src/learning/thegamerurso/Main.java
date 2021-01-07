3d8f69c7d11cea2d77f4b2e0f997bb21a3a5279e
Petros Kitsaras <petros.kitsaras@gmail.com>
2021-01-07 11:15:01 +0200

    finished

--- src/learning/thegamerurso/Main.java
+++ src/learning/thegamerurso/Main.java
@@ -18,7 +18,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(getDurationString(65,45));
-        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(3945L));
+        System.out.println(getDurationString(-41));
+        System.out.println(getDurationString(65,9));
     }
 
     public static String getDurationString(long minute,long seconds){
@@ -30,17 +32,17 @@ public class Main {
 
         String hourString = hours + "h ";
         if(hours < 10){
-            hourString= "0" + hours;
+            hourString = "0" + hours + "h ";
         }
 
         String remainingMinutesString = remainingMinutes + "m ";
         if(remainingMinutes < 10){
-            remainingMinutesString= "0" + remainingMinutes;
+            remainingMinutesString = "0" + remainingMinutes + "m ";
         }
 
         String secondsString = seconds + "s ";
         if(seconds < 10){
-            secondsString= "0" + seconds;
+            secondsString = "0" + seconds + "s ";
         }
         return hourString + remainingMinutesString + secondsString;
     }
